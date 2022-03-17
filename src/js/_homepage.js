8500e371e30efc09e8c5901b209ce31255b2120c
Tulika Das <tulikadas1029@gmail.com>
2022-03-17 21:13:36 +0530

    every section complete

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -1,2 +1,15 @@
+import { tns } from 'tiny-slider/src/tiny-slider'
 import GLightbox from 'glightbox';
 const lightbox = GLightbox({});
+
+if (document.querySelector('.top-bar')) {
+    tns({
+        container: '.top-bar .slides',
+        items: 1,
+        nav: false,
+        autoplay: true,
+        controls:false,
+        autoplayTimeout:3000,
+        autoplayButtonOutput: false,
+    })
+}
