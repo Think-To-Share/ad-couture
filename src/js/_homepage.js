6083efffc81426c3aeb920a4dbbd8ae642889130
Tulika Das <tulikadas1029@gmail.com>
2022-03-21 20:37:27 +0530

    responsive done and video section

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -13,3 +13,41 @@ if (document.querySelector('.top-bar')) {
         autoplayButtonOutput: false,
     })
 }
+
+const videoImage  = document.querySelector('.video-image');
+const video  = document.querySelector('.video');
+videoImage.addEventListener('click', event => {
+    if(window.innerWidth >= 768){
+        video.classList.remove('hide')
+        videoImage.classList.add('hide')
+        return;
+    }
+    const myGallery = GLightbox({
+        elements: [
+            {
+                'href': 'https://www.youtube.com/embed/JHJ9RZ6APAg',
+                'type': 'video',
+                'source': 'youtube', //vimeo, youtube or local
+                'width': 900,
+            },
+        ],
+        autoplayVideos: true,
+    });
+    myGallery.open();  
+})
+
+document.querySelector('#search-menu').classList.remove('toggled');
+
+document.querySelector('#search-icon').addEventListener('click',function(e) {
+    e.stopPropagation();
+    document.querySelector('#search-menu').classList.toggle('toggled');
+    document.querySelector("#popup-search").focus();
+});
+
+document.querySelector('#search-menu input').addEventListener('click', function(e) {
+    e.stopPropagation();
+});
+
+document.querySelector('#search-menu, body').addEventListener('click',function() {
+    document.querySelector('#search-menu').classList.remove('toggled');
+});
