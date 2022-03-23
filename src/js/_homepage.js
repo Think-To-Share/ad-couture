acb1830a128997205d2b5acff762fddeeca761ed
Tulika Das <tulikadas1029@gmail.com>
2022-03-23 14:26:02 +0530

    blog page and paggination add

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -15,39 +15,25 @@ if (document.querySelector('.top-bar')) {
 }
 
 const videoImage  = document.querySelector('.video-image');
-const video  = document.querySelector('.video');
-videoImage.addEventListener('click', event => {
-    if(window.innerWidth >= 768){
-        video.classList.remove('hide')
-        videoImage.classList.add('hide')
-        return;
-    }
-    const myGallery = GLightbox({
-        elements: [
-            {
-                'href': 'https://www.youtube.com/embed/JHJ9RZ6APAg',
-                'type': 'video',
-                'source': 'youtube', //vimeo, youtube or local
-                'width': 900,
-            },
-        ],
-        autoplayVideos: true,
-    });
-    myGallery.open();  
-})
-
-document.querySelector('#search-menu').classList.remove('toggled');
-
-document.querySelector('#search-icon').addEventListener('click',function(e) {
-    e.stopPropagation();
-    document.querySelector('#search-menu').classList.toggle('toggled');
-    document.querySelector("#popup-search").focus();
-});
-
-document.querySelector('#search-menu input').addEventListener('click', function(e) {
-    e.stopPropagation();
-});
-
-document.querySelector('#search-menu, body').addEventListener('click',function() {
-    document.querySelector('#search-menu').classList.remove('toggled');
-});
+if(videoImage) {
+    const video  = document.querySelector('.video');
+    videoImage.addEventListener('click', event => {
+        if(window.innerWidth >= 768){
+            video.classList.remove('hide')
+            videoImage.classList.add('hide')
+            return;
+        }
+        const myGallery = GLightbox({
+            elements: [
+                {
+                    'href': 'https://www.youtube.com/embed/JHJ9RZ6APAg',
+                    'type': 'video',
+                    'source': 'youtube', //vimeo, youtube or local
+                    'width': 900,
+                },
+            ],
+            autoplayVideos: true,
+        });
+        myGallery.open();  
+    })
+}
